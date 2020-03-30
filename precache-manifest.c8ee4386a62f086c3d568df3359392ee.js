7823620c62024b9468aa73f022df63003fad4e83
raymondfdavey <raymondfdavey@gmail.com>
2020-03-30 16:19:36 +0100

    Updates

---
+++ precache-manifest.c8ee4386a62f086c3d568df3359392ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e86375bda7129f02f5a0ea8c980c387",
+    "url": "/bees-in-space/index.html"
+  },
+  {
+    "revision": "01b7f952fa83129fc85c",
+    "url": "/bees-in-space/static/css/main.7e67293d.chunk.css"
+  },
+  {
+    "revision": "2eb5624ce348d1bbef90",
+    "url": "/bees-in-space/static/js/2.3b35bcac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bees-in-space/static/js/2.3b35bcac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01b7f952fa83129fc85c",
+    "url": "/bees-in-space/static/js/main.1bbe670c.chunk.js"
+  },
+  {
+    "revision": "bd00445e9aaa4bbf8536",
+    "url": "/bees-in-space/static/js/runtime-main.8f18d192.js"
+  },
+  {
+    "revision": "7323f5f0fe0ba82b2e86e50e9a6c0af9",
+    "url": "/bees-in-space/static/media/BEE-BCKRD.7323f5f0.png"
+  },
+  {
+    "revision": "f649074e4c475aadb2d097999fa7c72f",
+    "url": "/bees-in-space/static/media/danube.f649074e.mp3"
+  }
+]);
