516719532147bfa1f9243a7f8b5fcf3138df992b
raymondfdavey <raymondfdavey@gmail.com>
2020-03-30 13:00:41 +0100

    bees done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import bee from "./img/BEE-BCKRD.png";
+import space from "./img/SPACE.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { bees: null, beesFree: false };
+  render() {
+    const { bees, beesFree } = this.state;
+
+    return (
+      <div className={"content"}>
+        <div className={"beeContainer"}>
+          {bees &&
+            bees.map(eachBee => {
+              return <img src={bee} className={beesFree && "rotate"} />;
+            })}
+        </div>
+        <div className={"buttons"}>
+          <button onClick={() => this.addBee()}>BEES</button>
+          <button onClick={() => this.makeBeesFree()}>BEE FREE</button>
+        </div>
+      </div>
+    );
+  }
+
+  addBee = () => {
+    if (this.state.bees === null) {
+      return this.setState({ bees: ["bee"] });
+    }
+    this.setState({ bees: [...this.state.bees, "bee"] });
+  };
+  makeBeesFree = () => {
+    this.setState({ beesFree: !this.state.beesFree });
+  };
 }
 
 export default App;
